2769c9e688919c40720df2f293dfcbd694e69989
AxelineGit <saputrovalentinus@gmail.com>
2026-08-07 10:58:20 +0700

    fix nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//     eslint: {
-//     ignoreDuringBuilds: true,
-//   },  
-// };
-
-// export default nextConfig;
-
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
